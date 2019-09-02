206abaa8709e9c0aa1b70049bfd7326375740fb5
Louiskoyio <koyiolouis@gmail.com>
2019-09-02 16:27:03 +0300

    Edited Logic

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -7,35 +8,56 @@ public class App {
     public static void main(String[] args) {
 
         Cipher cipherText = new Cipher();
-        System.out.println("ENTER A MESSAGE: ");
+        System.out.println("ENTER TEXT TO BE ENCODED (FOR SENTENCES, TYPE WORD BY WORD MAX OF 5 WORDS");
         Scanner scanner = new Scanner(System.in);
         boolean inputOK = false;
         String userInput = "";
-        while (inputOK==false){
-            String warning = "(INPUT MUST ONLY BE LETTERS. NO SPACES, SYMBOLS OR NUMBERS)";
+        int i = 0;
+        ArrayList <String> userInputs = new ArrayList<String>();
+        while (!inputOK || "X".equals(userInput) == true ){
+            String warning = "(BAD INPUT!!\nINPUT MUST ONLY BE LETTERS. NO SPACES, SYMBOLS OR NUMBERS)";
             System.out.println(warning);
             userInput = scanner.nextLine();
             inputOK = cipherText.validateInput(userInput);
-
+            userInputs.add(userInput);
+            i++;
         }
 
-            System.out.println("Enter decryption key:\n(MUST BE BETWEEN 1-26)");
-            int key = scanner.nextInt();
+        System.out.println("Enter decryption key:\n(MUST BE BETWEEN 1-26)");
+        int key = scanner.nextInt();
+        System.out.print("Input:  " );
+        for(String word : userInputs){
 
+            System.out.print(word + " " );
+            }
 
-            String encodedText = cipherText.runCipher(userInput,key);
-            Decipher decipherText = new Decipher();
-            String decodedText = decipherText.runDecipher(encodedText,key);
+        ArrayList <String> encodedWords = new ArrayList<String>();
+        ArrayList <String> decodedWords = new ArrayList<String>();
+            for(String word: userInputs){
+                String encodedText = cipherText.runCipher(word,key);
+                Decipher decipherText = new Decipher();
+                String decodedText = decipherText.runDecipher(encodedText,key);
 
-            System.out.println("INPUT: " + userInput );
-            System.out.println("ENCODED: " + encodedText );
-            System.out.println("DECODED: " + decodedText );
+                encodedWords.add(encodedText);
+                decodedWords.add(decodedText);
 
+            }
 
+        System.out.print("\nEncoded String:  " );
+        for(String word : encodedWords){
 
-
-
-
+            System.out.print(word + " " );
         }
+        System.out.print("\nDecoded String:  " );
+        for(String word : decodedWords){
+
+            System.out.print(word + " " );
+        }
+
+
+
+
+
+    }
     }
 
